1d84618124dc0db3e1197763d9c88ea2dd4fca7c
kapilRohilla <kapilrohilla2002@gmail.com>
2023-01-12 17:34:34 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const docResult = document.getElementsByTagName('span');
+let btn0 = document.getElementsByTagName('li')[0];
+let btn1 = document.getElementsByTagName('li')[1];
+let btn2 = document.getElementsByTagName('li')[2];
+let btn3 = document.getElementsByTagName('li')[3];
+let btn4 = document.getElementsByTagName('li')[4];
+
+console.log(btn);
